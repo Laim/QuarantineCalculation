fec1edecd1a5255e92e8f1ce45d8715dfd3042de
Laim <laimmckenzie@icloud.com>
2019-02-04 12:04:58 +0000

    Update index.php
    Added Underutilized date

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-    if(isset($_GET['d']) && isset($_GET['q'])) {
+    if(isset($_GET['d']) && isset($_GET['q']) && isset($_GET['u'])) {
         $delete = htmlentities($_GET['d']); //30 days
         $quaran = htmlentities($_GET['q']); //21 days
+        $underu = htmlentities($_GET['u']); //60 days
         $default = false;
     } else {
-        $delete = "30";
-        $quaran = "15";
+        $delete = "90";
+        $quaran = "60";
+        $underu = "30";
         $default = true;
     }
 
@@ -35,7 +37,7 @@
         <div class="row mt-5">
             <div class="col">
                 <div class="alert alert-danger" role="alert">
-                    No timeframe has been supplied, default dates are being used. (Quar: 30, Del: 15)
+                    No timeframe has been supplied, default dates are being used. (Quar: <?php print($quaran);?>, Del: <?php print($delete);?>, Underu: <?php print($underu);?>)
                 </div>
             </div>
         </div>
@@ -56,6 +58,17 @@
                         <h5 class="card-title">Quarantine Date</h5>
                         <p class="card-text">Device's from this date and before should be in Quarantine.</p>
                         <p class="card-text"><?php print(date('d-m-Y', strtotime("-" . $quaran . " days")));?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Underutilized Date</h5>
+                        <p class="card-text">Software underutilized date (software last used on this date should be removed)</p>
+                        <p class="card-text"><?php print(date('d-m-Y', strtotime("-" . $underu . " days")));?></p>
                     </div>
                 </div>
             </div>
